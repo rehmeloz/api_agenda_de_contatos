74f3a4dc8607946ac93e1591c96e6d5beebb48be
rehmeloz <renatamelo.pro@outlook.com>
2026-06-15 16:41:36 -0300

    adiciona campo sobrenome em dto's de atualização de contato e criação de contato

--- API_AgendaDeContatos/DTOs/CriarContatoDto.cs
+++ API_AgendaDeContatos/DTOs/CriarContatoDto.cs
@@ -5,6 +5,7 @@ namespace API_AgendaDeContatos.DTOs;
 public class CriaContatoDto
 {
     public string Nome { get; set; } = string.Empty;
+    public string Sobrenome { get; set; } = string.Empty;
     public string Telefone { get; set; } = string.Empty;
     public string Email { get; set; } = string.Empty;
     public bool Favorito { get; set; }
