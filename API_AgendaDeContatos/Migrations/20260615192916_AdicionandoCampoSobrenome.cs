a281928429693fc1178def81adb8a11fc718716c
rehmeloz <renatamelo.pro@outlook.com>
2026-07-16 16:29:33 -0300

    Adiciona últimas migrations

---
+++ API_AgendaDeContatos/Migrations/20260615192916_AdicionandoCampoSobrenome.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API_AgendaDeContatos.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdicionandoCampoSobrenome : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Telefone",
+                table: "Contatos",
+                type: "TEXT",
+                maxLength: 11,
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Nome",
+                table: "Contatos",
+                type: "TEXT",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Contatos",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Sobrenome",
+                table: "Contatos",
+                type: "TEXT",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Sobrenome",
+                table: "Contatos");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Telefone",
+                table: "Contatos",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldMaxLength: 11);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Nome",
+                table: "Contatos",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldMaxLength: 100);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Contatos",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+        }
+    }
+}
