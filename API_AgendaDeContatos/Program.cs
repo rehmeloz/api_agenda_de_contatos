5e9392f52a4597fad377aaadb1b184a6b5dec36e
rehmeloz <renatamelo.pro@outlook.com>
2026-07-16 16:30:07 -0300

    Praticando Minimal API

--- API_AgendaDeContatos/Program.cs
+++ API_AgendaDeContatos/Program.cs
@@ -1,11 +1,14 @@
 using API_AgendaDeContatos.Data;
+using API_AgendaDeContatos.DTOs;
+using API_AgendaDeContatos.Enums;
+using API_AgendaDeContatos.Models;
 using API_AgendaDeContatos.Repositories;
 using API_AgendaDeContatos.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
+//builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -24,10 +27,49 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+// Praticando MinimalAPI
+
+// GET - Buscar todos
+app.MapGet("/contatos", async (AppDbContext db) =>
+    await db.Contatos.ToListAsync());
+
+// POST - Adicionar
+app.MapPost("/novoContato", async (AppDbContext db, Contato contato) => {
+    db.Contatos.Add(contato);
+    await db.SaveChangesAsync();
+    return Results.Created($"/novoContato/{contato.Id}", contato);
+});
+
+// PUT - Atualizar
+app.MapPut("/atualizar{id}", async (AppDbContext db, AtualizaContatoDto contatoDto, int id) => {
+    var contatoASertualizado = await db.Contatos.FindAsync(id);
+    if (contatoASertualizado is null) return Results.NotFound();
+
+    contatoASertualizado.Nome = contatoDto.Nome;
+    contatoASertualizado.Telefone = contatoDto.Telefone;
+    contatoASertualizado.Email = contatoDto.Email;
+    contatoASertualizado.Favorito = contatoDto.Favorito;
+    contatoASertualizado.Categoria = contatoDto.Categoria;
+    contatoASertualizado.Sobrenome = contatoDto.Sobrenome;
+
+    await db.SaveChangesAsync();
+    return Results.Ok("Contato atualizado");
+});
+
+// DELETE - Deletar
+app.MapDelete("/deletar", async (AppDbContext db, int id) => {
+    var contato = await db.Contatos.FindAsync(id);
+    if (contato is null) return Results.NotFound();
+
+    db.Contatos.Remove(contato);
+    await db.SaveChangesAsync();
+    return Results.Ok("Contato deletado");
+});
+
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
+//app.UseAuthorization();
 
-app.MapControllers();
+//app.MapControllers();
 
 app.Run();
